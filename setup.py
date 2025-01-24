a69411672e0a46c75c7da8b00c9491fcf3eb113a
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2025-01-23 20:40:35 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="staticvectors",
-    version="0.1.0",
+    version="0.2.0",
     author="NeuML",
     description="Work with static vector models",
     long_description=DESCRIPTION,
